bb2686520854f180e6546e999187cc394d4fa231
Harsh Sharma <harshsharma5183@gmail.com>
2026-03-26 16:01:05 +0530

    Update BudgetSettings.jsx

--- frontend/src/pages/BudgetSettings.jsx
+++ frontend/src/pages/BudgetSettings.jsx
@@ -1,4 +1,4 @@
-// src/pages/BudgetSettings.jsx
+
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
